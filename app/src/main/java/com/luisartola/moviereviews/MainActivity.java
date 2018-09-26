95d445c631e06dd18cacfbe5e9f2a3e65eb2bd8d
Luis Artola <a@luisartola.com>
2018-09-25 23:55:51 -0700

    Initial app skeleton.

---
+++ app/src/main/java/com/luisartola/moviereviews/MainActivity.java
@@ -0,0 +1,13 @@
+package com.luisartola.moviereviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
